c7a79473518c5cd916e2c124c65a805960af75d8
David Allsopp <david.allsopp@metastack.com>
2024-12-28 14:29:10 +0000

    WIP: missing parse case in directives
    Need to check this slightly obtuse code through and expand this message,
    but basically there's a directive with 64 inside it which needs to be
    parsed. There was a recent PR on this which may have included reference
    to other documentation for the valid character set here?!

--- coff.ml
+++ coff.ml
@@ -695,7 +695,7 @@ module Coff = struct
       | _ -> raise Exit
     and aux1 i0 i = if i = l then (String.sub s i0 (i - i0), [])::[]
     else match s.[i] with
-      | 'a'..'z' | 'A'..'Z' | '-' -> aux1 i0 (i+1)
+      | 'a'..'z' | 'A'..'Z' | '0'..'9' | '-' -> aux1 i0 (i+1)
       | ' ' -> (String.sub s i0 (i - i0), []) :: aux0 (i+1)
       | ':' -> aux2 (String.sub s i0 (i - i0)) [] (i+1)
       | _   -> raise Exit
